05cb5a0300d983b42248d3da51482e757f36a786
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-08-31 17:00:17 -0500

    Added comments to blogposts

--- blogposts/models.py
+++ blogposts/models.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -5,14 +6,13 @@ from django.urls import reverse
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True, unique=True)
     updated_on = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
-
     )
 
     def __str__(self) -> str:
@@ -20,3 +20,21 @@ class BlogPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('blogpost_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    blogpost = models.ForeignKey(
+        BlogPost,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        )
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('blogpost_list')
