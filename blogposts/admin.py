9609600e5e3b9d2a2fef1fd2e68695e0c4a999c8
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-08-31 17:00:17 -0500

    Added comments to blogposts

--- blogposts/admin.py
+++ blogposts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, Comment
 
-admin.site.register(BlogPost)
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+class BlogPostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Comment)
 
