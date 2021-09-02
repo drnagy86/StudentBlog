bd3904c39843edf95d164373fe39c2ffc0524234
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-09-01 22:22:13 -0500

    Comments form added to Blogpost detail view

--- blogposts/urls.py
+++ blogposts/urls.py
@@ -7,13 +7,14 @@ from .views import (
     BlogPostDetialView,
     BlogPostUpdateView,
     BlogPostCreateView,
+    BlogPostCommentView
     
 )
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='blogpost_list'),
     path('<int:pk>/edit/', BlogPostUpdateView.as_view(), name='blogpost_edit'),
-    path('<int:pk>/', BlogPostDetialView.as_view(), name='blogpost_detail'),
+    path('<int:pk>/', BlogPostCommentView.as_view(), name='blogpost_detail'),
     path('<int:pk>/delete/', BlogPostDeleteView.as_view(), name='blogpost_delete'),
     path('new/', BlogPostCreateView.as_view(), name='blogpost_new'),
 ]
