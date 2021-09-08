1cbea3727f132925a79a3c8fb687febf3e67d67b
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-09-07 21:31:53 -0500

    Added about page, cleaned up nav bar

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-    path('blogposts/', include('blogposts.urls'))
+    path('blogposts/', include('blogposts.urls')),
+
 ]
