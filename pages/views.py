51a15707e6b140c701aff1442edaeb813beb9c47
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-09-23 12:09:03 -0500

    front page upgrade

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+from blogposts.models import BlogPost
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = BlogPost
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
